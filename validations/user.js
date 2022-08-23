82fe84fbb0ffb2a2e3e2d8f351187aec699d2e8d
AjaxGroup1400 <uplayggboy@gmail.com>
2022-08-23 17:06:32 +0430

    added validation for user sign in and sign up

---
+++ validations/user.js
@@ -0,0 +1,40 @@
+const { body } = require("express-validator");
+
+const generateValidation = (field) =>
+  body(field)
+    .trim()
+    .isString()
+    .withMessage(`not a valid ${field}`)
+    .not()
+    .isEmpty()
+    .withMessage(`${field} can't be empty.`)
+    .isAlpha()
+    .withMessage(`${field} should only contains letters.`);
+
+const email = body("email")
+  .trim()
+  .normalizeEmail()
+  .escape()
+  .isEmail()
+  .withMessage("not a valid email");
+
+const password = body("password")
+  .trim()
+  .isString()
+  .withMessage("not a valid password")
+  .not()
+  .isEmpty()
+  .withMessage("password can't be empty")
+  .isStrongPassword()
+  .withMessage("not a strong password");
+
+const validateSignUp = [
+  generateValidation("firstname"),
+  generateValidation("lastname"),
+  email,
+  password,
+];
+
+const validateSignIn = [email, password];
+
+module.exports = { validateSignUp, validateSignIn };
